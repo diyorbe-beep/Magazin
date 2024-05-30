cbd64686bf323dc69aafa1f702f4ccd67b6a028e
dior_coder <144696383+diyorbe-beep@users.noreply.github.com>
2024-05-30 19:12:49 +0500

    awd

--- js/home.js
+++ js/home.js
@@ -4,6 +4,9 @@ const close = document.querySelector('.close')
 const dowinCard1 = document.querySelectorAll('.dowin_card_1')
 const toCard = document.querySelectorAll('.toCard')
 const _1 = document.querySelectorAll('._1')
+const top_select = document.querySelector('.top_select')
+const top_p = document.querySelector('.top_p')
+const top_a = document.querySelector('.top_a')
 bars.addEventListener('click', () => {
       resNav.classList.toggle('active')
 })
@@ -11,3 +14,21 @@ bars.addEventListener('click', () => {
 close.addEventListener('click', () => {
       resNav.classList.toggle('active')
 })
+
+top_select.addEventListener('change',  ()=>{
+      translet()
+      translet1()
+})
+
+
+function translet() {
+      if (top_select.value = 'Russion') {
+            top_p.textContent = `Летняя распродажа на все купальники и бесплатная экспресс-доставка — СКИДКА 50%!`
+      }
+}
+
+function translet1() {
+      if (top_select.value = 'Uzbekistan') {
+            top_p.textContent = `Barcha cho'milish kostyumlari uchun yozgi sotuv va bepul tezkor yetkazib berish - 50% chegirma!`
+      }
+}
