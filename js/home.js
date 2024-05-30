952fb2b80e2c651c2f1d5948084b39e7e49ca35b
dior_coder <144696383+diyorbe-beep@users.noreply.github.com>
2024-05-30 17:03:14 +0500

    daads

--- js/home.js
+++ js/home.js
@@ -1,16 +1,13 @@
 const resNav = document.querySelector('.resNav')
 const bars = document.querySelector('.bars')
-const close =document.querySelector('.close')
+const close = document.querySelector('.close')
 const dowinCard1 = document.querySelectorAll('.dowin_card_1')
 const toCard = document.querySelectorAll('.toCard')
-bars.addEventListener('click', ()=>{
+const _1 = document.querySelectorAll('._1')
+bars.addEventListener('click', () => {
       resNav.classList.toggle('active')
 })
 
-close.addEventListener('click', ()=>{
+close.addEventListener('click', () => {
       resNav.classList.toggle('active')
-})
-
-dowinCard1.addEventListener('mouseenter', ()=>{
-      toCard.classList.add('toCard_active')
 })
